81d5f2335f64cb1796b3879b7ec1db62d8b170eb
Ezell <lynellf@gmail.com>
2021-12-26 10:46:10 -0600

    allow for option type to persist through flatMap chain

--- src/__test__/option.test.ts
+++ src/__test__/option.test.ts
@@ -1,5 +1,17 @@
 import { Option } from '../option'
 
+function toSet<T>(iterable: Iterable<T>): Set<T> {
+  return new Set(iterable)
+}
+
+function toArray<T>(iterable: Iterable<T>): T[] {
+  return Array.from(iterable)
+}
+
+function divideBy(y: number) {
+  return (x: number) => Option(y === 0 ? null : x / y)
+}
+
 describe('option type', () => {
   it('should return undefined', () => {
     const result = Option(null)
@@ -9,5 +21,23 @@ describe('option type', () => {
   it('should return a value', () => {
     const result = Option(1)
     expect(result.value).toBe(1)
+  })
+
+  it('unwraps a nested option value', () => {
+    const divideByFour = divideBy(4)
+    const five = Option(20).flatMap(divideByFour).value
+    expect(five).toBe(5)
+  })
+
+  it('can map an option type', () => {
+    const divideByFour = divideBy(4)
+    const six = divideByFour(20).map((x) => x! + 1).value
+    expect(six).toBe(6)
+  })
+
+  it('maintains type through the chain', () => {
+    const arr = Option([1, 2, 3, 3])
+    const result = arr.flatMap(toSet).flatMap(toArray).value
+    expect(result).toStrictEqual([1, 2, 3])
   })
 })
--- src/some.ts
+++ src/some.ts
@@ -3,11 +3,25 @@ import { tap } from './tap'
 import { flatMap } from './flatmap'
 import type { Some as TSome } from './types'
 
+function unwrap<T>(x: NonNullable<T> | TSome<NonNullable<T>>): NonNullable<T> {
+  const isSome = (x as TSome<NonNullable<T>>)?.type === 'Some'
+  const value = (x as TSome<NonNullable<T>>)?.value
+  const hasValue = value !== undefined
+
+  if (!isSome) return x as NonNullable<T>
+  if (!hasValue) return x as NonNullable<T>
+
+  return hasValue ? value : unwrap(x)
+}
+
 export function Some<T>(x: NonNullable<T>): TSome<T> {
   return {
-    value: x,
+    type: 'Some',
     map: map(x),
     flatMap: flatMap(x),
-    tap: tap(x)
+    tap: tap(x),
+    get value() {
+      return unwrap(x)
+    }
   }
 }
