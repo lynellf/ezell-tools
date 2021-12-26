b73540ecce15621ad84cf9cc7514e5d75ba0b577
Ezell <lynellf@gmail.com>
2021-12-26 10:55:52 -0600

    forgot to build

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,10 +1,12 @@
 export interface None<T> {
+    type: 'None';
     value: undefined;
     map: (fn: Function) => None<T>;
-    flatMap: (fn: Function) => None<T>;
+    flatMap: <U>(fn: (x: T) => U) => Option<U>;
     tap: (fn: Function) => None<T>;
 }
 export interface Some<T> {
+    type: 'Some';
     value: T;
     map: <U>(fn: (x: T) => NonNullable<U>) => Some<U>;
     flatMap: <U>(fn: (x: NonNullable<T>) => U) => Option<U>;
